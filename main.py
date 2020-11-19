52dcad24d010da6f70f36424430ca7e16220ba62
Iampato <858wpwaweru@gmail.com>
2020-11-19 22:55:59 +0300

    added makefile

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from config.connect import SmartMeterConfig
+
 
 def main():
-    
+    smartMeterConfig = SmartMeterConfig()
+    print(smartMeterConfig.database_user)
+
 
 if __name__ == "__main__":
     main()
-    
